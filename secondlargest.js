0be8a5db3f617cf4f6c87ca45d15239e24884450
joprin <joprinjoy3@gmail.com>
2024-06-25 17:36:10 +0530

    Second largest number -Removed duplicates -converted set to array -result obtained

--- secondlargest.js
+++ secondlargest.js
@@ -1,4 +1,9 @@
 let digits = [2,4,5,6,8,1,7]
+//converting to set type to remove duplicates
+digits = new Set(digits)
+// converting from set type to array type
+digits = Array.from(digits)
+//sorting the array in descending order using arrow function
 let digit_sorted = digits.sort((a,b)=>{
     return b - a
 })
